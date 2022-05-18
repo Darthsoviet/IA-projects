2608e290c7981934e57b2e60269bffe717067ad0
ivan Lujan <hawkgoldfes@gmail.com>
2022-05-18 17:16:03 -0500

    entry point added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import core.Labyrinth;
+import util.LabyrinthGenerator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
 
   public static void main(String[] args) {
+
+    final LabyrinthGenerator labyrinthGenerator = new LabyrinthGenerator(30);
+
+    final List<Labyrinth> laberinthList =
+            IntStream.rangeClosed(0, 10)
+                    .mapToObj(s -> labyrinthGenerator.generate(30, 30))
+                    .collect(Collectors.toUnmodifiableList());
+
+    laberinthList.forEach(Labyrinth::solveAndPrint);
 
 
   }
